7e50713ca96e153be91391bea9f7b903cf6fadf7
getzabel <getzabel1@gmail.com>
2019-11-21 18:32:18 +0000

    initial commit

---
+++ Variables.js
@@ -0,0 +1,41 @@
+// single line comment
+
+/*
+ multi line comment
+*/
+
+var name = 'Alan';
+
+// mutable variable - can be changed
+
+let age = 37; // this variable is declared and intialized
+
+console.log(age);
+
+// Inmutable variable - can't be changed - preferred over let
+
+const hairColor = 'black';
+
+console.log(hairColor);
+
+
+// declare a varible
+let country;
+
+// initialize a variable (assign it a value)
+country = 'Ireland';
+
+
+// variable scope - read up on this -> https://www.tutorialspoint.com/es6/es6_variables.htm
+// 1. global scope variables
+// 2. local scope variables
+
+age = 32;
+
+console.log(age)
+
+
+
+
+
+
