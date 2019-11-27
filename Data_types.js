83fa1a76643f10b0c21f4e17c708a3e0b028e777
getzabel <getzabel1@gmail.com>
2019-11-27 22:28:52 +0000

    added data types

---
+++ Data_types.js
@@ -0,0 +1,18 @@
+// Data Types
+
+// Primitive Types
+
+//Boolean 
+true
+false
+
+//Null (It's just a special value which represents "nothing", "empty", or "value unknown")
+null 
+
+//String (must be surrounded by quotes)
+'Hello World'
+
+//Number (represents both integer and floating point numbers)
+1, 2, 4.5
+
+//NAN ( represents a computational eror)
