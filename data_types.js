aee38d6637356fa8c900542996683d8d7f69a103
getzabel <getzabel1@gmail.com>
2019-11-29 16:07:34 +0000

    added definitions

--- data_types.js
+++ data_types.js
@@ -22,6 +22,14 @@ console.log(a); //undefined
 
 
 
-// Object 
+/* Object (The object literal is a simple way of creating an object using { } brackets. 
+You can include key-value pair in { }, where key would be property or method name 
+and value will be value of property of any data type or a function.
+Use comma (,) to separate multiple key-value pairs.) 
+*/ 
+let <object-name> = { key1: value1, key2: value2,... keyN: valueN};
 
-// Symbol
+
+
+// Symbols (are new primitive type introduced in ES6. Symbols are completely unique identifiers. )
+const symbol = Symbol('description')
